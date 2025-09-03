c7d80ce051c35a33462db03c647b04386a49c52d
chernyxa <gosha_i2002@mail.ru>
2025-09-03 14:57:19 +0300

    commit1

---
+++ src/main/java/com/igor/mdm/config/MdmConfig.java
@@ -0,0 +1,59 @@
+package com.igor.mdm.config;
+
+import com.igor.mdm.property.MdmProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.*;
+
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+public class MdmConfig {
+
+    private final MdmProperty mdmProperty;
+
+    @Bean(name = "serviceOneExecutor")
+    public Executor serviceOneCallExecutor() {
+        return createElasticExecutor(mdmProperty.processingPool().serviceOne().threads(),
+                mdmProperty.processingPool().serviceOne().queueDepth());
+    }
+
+    @Bean(name = "serviceTwoExecutor")
+    public Executor serviceTwoCallExecutor() {
+        return createElasticExecutor(mdmProperty.processingPool().serviceTwo().threads(),
+                mdmProperty.processingPool().serviceTwo().queueDepth());
+    }
+
+    @Bean
+    public ExecutorService retrySendMessagesExecutor() {
+        return new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(1), new ThreadPoolExecutor.DiscardPolicy());
+    }
+
+    @Bean
+    public ExecutorService deleteOldMessagesExecutor() {
+        return new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(1), new ThreadPoolExecutor.DiscardPolicy());
+    }
+
+    private ThreadPoolExecutor createElasticExecutor(int threads, int queueCapacity) {
+        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(queueCapacity);
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                threads, threads,
+                60L, TimeUnit.SECONDS,
+                queue,
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+
+        threadPoolExecutor.allowCoreThreadTimeOut(true);
+
+        return threadPoolExecutor;
+    }
+
+}
