d564fcc603bd738b0b0c0a4f0d5c59fded5d261e
chernyxa <gosha_i2002@mail.ru>
2025-09-16 06:33:14 +0300

    commit1

---
+++ src/main/java/com/igor/mdm/dto/ResponseDataDto.java
@@ -0,0 +1,17 @@
+package com.igor.mdm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class ResponseDataDto {
+    private String response;
+    private List<String> errors;
+}
---
+++ src/main/java/com/igor/mdm/dto/ServiceOneUpdatePhoneRequestDto.java
@@ -0,0 +1,25 @@
+package com.igor.mdm.dto;
+
+import lombok.Builder;
+
+import java.util.UUID;
+
+@Builder
+public record ServiceOneUpdatePhoneRequestDto(
+        Meta meta,
+        Body body
+) {
+
+    @Builder
+    public record Meta(
+            String systemId,
+            String sender
+    ) { }
+
+    @Builder
+    public record Body(
+            UUID id,
+            String guid,
+            String phone
+    ) { }
+}
