5089aacedb5f88c43a0a4839882dd9f1bde17698
chernyxa <gosha_i2002@mail.ru>
2025-09-17 06:49:21 +0300

    commit1

---
+++ src/main/java/com/igor/mdm/model/MdmMessage.java
@@ -0,0 +1,47 @@
+package com.igor.mdm.model;
+
+import com.igor.mdm.enums.MdmEventType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class MdmMessage extends AuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID externalId;
+
+    private String guid;
+
+    @Enumerated(EnumType.STRING)
+    private MdmEventType type;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private String payload;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MdmMessage mdmMessage = (MdmMessage) o;
+        return Objects.equals(id, mdmMessage.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
